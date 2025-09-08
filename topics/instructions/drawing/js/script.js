209a08f900fa5a2789a968001b14001e24536063
Shrook12 <shrook1@gmail.com>
2025-09-08 14:11:38 -0400

    Implemented the greatest record of all time

--- topics/instructions/drawing/js/script.js
+++ topics/instructions/drawing/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Drawing
+ * The Greatest Record of All Time
  * Shrook
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Displays the Greatest Record of All Time
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a square CANVAS 
 */
 function setup() {
+    createCanvas(640, 640);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the record
 */
 function draw() {
+    //grey background
+    background(150);
+
+    //the main part of the record
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
+
+    //the label on the record
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140);
+    pop();
+
+    //The hole in the record
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
 
 }
