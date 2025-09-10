f1ef1d10d9090ad7eb9a968cde7b814d3d22b8df
Shrook12 <shrook1@gmail.com>
2025-09-10 11:51:44 -0400

    Started the instruction challenge

---
+++ topics/instruction_challenge/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Instruction Challenge
+ * Shrook Ahmed
+ * 
+ * This is the instruction challenge where I have to draw a landscape.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(920, 700);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+    background(0, 0, 139);
+
+}
