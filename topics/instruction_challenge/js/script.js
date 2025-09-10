87ab8b145980ecd7596b34997a5989429f8af8c7
Shrook12 <shrook1@gmail.com>
2025-09-10 13:30:09 -0400

    did a sky, a land, and a moon for the instruction challenge but still working on it.

--- topics/instruction_challenge/js/script.js
+++ topics/instruction_challenge/js/script.js
@@ -23,4 +23,44 @@ function draw() {
 
     background(0, 0, 139);
 
+    push();
+    fill("#e0e0e0ff");
+    rect(0, 400, 920, 320);
+    pop();
+
+    push();
+    fill("#a7a7a7ff");
+    noStroke();
+    ellipse(100, 600, 30, 30);
+    ellipse(300, 450, 80, 80);
+    ellipse(550, 480, 60, 60);
+    ellipse(600, 650, 40, 40);
+    ellipse(700, 580, 50, 50);
+    ellipse(900, 520, 70, 70);
+
+    pop();
+
+    push();
+    fill("#c3c3c3ff");
+    noStroke();
+    ellipse(10, 500, 60, 60);
+    ellipse(250, 650, 100, 100);
+    ellipse(450, 550, 70, 70);
+    ellipse(750, 480, 120, 120);
+    pop();
+
+
+
+    push();
+    fill("#d490ffff");
+    noStroke();
+    ellipse(220, 160, 180, 180);
+    pop();
+
+    push();
+    fill("#e0adffff");
+    noStroke();
+    ellipse(240, 160, 140, 140);
+    pop();
+
 }
