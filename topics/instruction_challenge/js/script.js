8acc8c9954c2a08bec4b78d82efce924ec6f6af4
Shrook12 <shrook1@gmail.com>
2025-09-10 19:14:34 -0400

    Organized and commented on the instruction challenge and finished the challenge

--- topics/instruction_challenge/js/script.js
+++ topics/instruction_challenge/js/script.js
@@ -3,6 +3,7 @@
  * Shrook Ahmed
  * 
  * This is the instruction challenge where I have to draw a landscape.
+ * It's an imaginary landscape in the moon where an extraterrestrial bird live.
  */
 
 "use strict";
@@ -17,12 +18,26 @@ function setup() {
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * this is to draw the my landscape with a sky, a land, a planet, a house and a creature.
 */
 function draw() {
 
+    drawSky();
+    drawLand();
+    drawPlanet();
+    drawHouse();
+    drawCreature();
+
+}
+/** draw the sky and the stars */
+function drawSky() {
+    /**
+     * this is the background
+     * color which is the sky */
+
     background(0, 0, 139);
-    //some stars
+
+    //some circles for the stars
     push();
     fill("#ffffffff");
     noStroke();
@@ -33,15 +48,21 @@ function draw() {
     ellipse(450, 150, 4, 4);
     ellipse(600, 50, 5, 5);
     ellipse(900, 100, 7, 7);
-
     pop();
+
+}
+/** this is to draw
+ * the land and some circles inside. The land is on the moon:)
+ * */
+function drawLand() {
     //draw land
     push();
     fill("#e0e0e0ff");
     noStroke();
     rect(0, 400, 920, 320);
     pop();
-    //draw circles in land
+
+    //draw circles in land for the moon texture
     push();
     fill("#a7a7a7ff");
     noStroke();
@@ -63,149 +84,196 @@ function draw() {
     ellipse(750, 480, 120, 120);
     pop();
 
+}
 
-    //draw moon
+
+/**draw  another planet that we can see from the moon*/
+
+function drawPlanet() {
+
+    // big circle
     push();
     fill("#d490ffff");
     noStroke();
     ellipse(220, 160, 180, 180);
     pop();
 
+    // a smaller circle
     push();
     fill("#e0adffff");
     noStroke();
     ellipse(240, 160, 140, 140);
     pop();
+}
 
-    //house
+/** this is to draw the house and 
+ * the other components of a house */
+function drawHouse() {
+    drawBody();
+    drawDoor();
+    drawWindow();
+    drawRoof();
 
-    push();
-    fill("#ffdb78ff");
-    noStroke();
-    rect(470, 150, 200, 250);
-    pop();
-    push();
-    fill("#ffe59fff");
-    noStroke();
-    rect(470, 150, 170, 220);
-    pop();
-    //house door
-    push();
-    fill("#ed7070ff");
-    noStroke();
-    rect(520, 270, 100, 130);
-    pop();
-    push();
-    push();
-    fill("#945151ff");
-    noStroke();
-    rect(520, 370, 100, 30);
-    pop();
-    push();
-
-    push();
-    fill("#ffbf3fff");
-    noStroke();
-    ellipse(600, 340, 10, 10);
-    pop();
-
-    //windows
-    push();
-    fill("#ed7070ff");
-    noStroke();
-    ellipse(570, 210, 90, 90);
-    pop();
-
-    push();
-    fill("#adc1ffff");
-    noStroke();
-    ellipse(570, 210, 70, 70);
-    pop();
-
-    push();
-    fill("#ed7070ff");
-    noStroke();
-    triangle(468, 151, 572, 70, 672, 151);
-    pop();
-
-    //home roff
-
-    push();
-    fill("#c7d5ffff");
-    noStroke();
-    ellipse(570, 210, 50, 50);
-    pop();
+    /**draw the main body of the house */
+    function drawBody() {
+        push();
+        fill("#ffdb78ff");
+        noStroke();
+        rect(470, 150, 200, 250);
+        pop();
+        push();
+        fill("#ffe59fff");
+        noStroke();
+        rect(470, 150, 170, 220);
+        pop();
+    }
+    /**draw the door of the house and the doorknob */
+    function drawDoor() {
+        //draw door
+        push();
+        fill("#ed7070ff");
+        noStroke();
+        rect(520, 270, 100, 130);
+        pop();
+        push();
+        push();
+        fill("#945151ff");
+        noStroke();
+        rect(520, 370, 100, 30);
+        pop();
+        push();
+        // draw doorknob
+        push();
+        fill("#ffbf3fff");
+        noStroke();
+        ellipse(600, 340, 10, 10);
+        pop();
+    }
 
 
-    //darw bird alien
+    /** draw the window of the house*/
+    function drawWindow() {
+        //the outside of the window
+        push();
+        fill("#ed7070ff");
+        noStroke();
+        ellipse(570, 210, 90, 90);
+        pop();
 
-    //leg1 
-    push();
-    fill("#d29a00ff");
-    noStroke();
-    rect(705, 300, 10, 100);
-    rect(705, 380, 50, 20);
-    pop();
+        //the inside1 of the window
+        push();
+        fill("#adc1ffff");
+        noStroke();
+        ellipse(570, 210, 70, 70);
+        pop();
 
-    push();
-    fill("#ffbb00ff");
-    noStroke();
-    rect(675, 300, 10, 100);
-    rect(675, 380, 50, 20);
-    pop();
+        //the inside2 of the window
 
-    push();
-    fill("#44cbcbff");
-    noStroke();
-    ellipse(700, 260, 100, 100);
-    pop();
+        push();
+        fill("#67708bff");
+        noStroke();
+        ellipse(570, 210, 50, 50);
+        pop();
+    }
+    /** draw the house roof*/
+    function drawRoof() {
+        push();
+        fill("#ed7070ff");
+        noStroke();
+        triangle(468, 151, 572, 70, 672, 151);
+        pop();
 
-    push();
-    fill("#52d5d5ff");
-    noStroke();
-    ellipse(700, 260, 80, 80);
-    pop();
-
-
-    //eyes1
-    push();
-    fill("#ffffffff");
-    noStroke();
-    ellipse(675, 255, 25, 25);
-    pop();
-
-    push();
-    fill("#000000ff");
-    noStroke();
-    ellipse(675, 255, 15, 15);
-    pop();
-
-    //eye2
-    push();
-    fill("#ffffffff");
-    noStroke();
-    ellipse(725, 255, 25, 25);
-    pop();
-
-    push();
-    fill("#000000ff");
-    noStroke();
-    ellipse(725, 255, 15, 15);
-    pop();
-
-    //bird alien nose
-    push();
-    fill("#ffd500ff");
-    noStroke();
-    rect(695, 265, 30, 10);
-    pop();
-
-
-
-
-
-
-
-
+    }
 
 }
+
+
+/**draw bird alien(creature) */
+
+function drawCreature() {
+
+    drawLegs();
+    drawBody();
+    drawEyes();
+    drawNose();
+
+    /**draw the legs of the creature */
+    function drawLegs() {
+        //leg one
+        push();
+        fill("#d29a00ff");
+        noStroke();
+        rect(705, 300, 10, 100);
+        rect(705, 380, 50, 20);
+        pop();
+
+        // leg 2
+        push();
+        fill("#ffbb00ff");
+        noStroke();
+        rect(675, 300, 10, 100);
+        rect(675, 380, 50, 20);
+        pop();
+    }
+
+    /**draw the main body of our creature */
+    function drawBody() {
+        push();
+        fill("#44cbcbff");
+        noStroke();
+        ellipse(700, 260, 100, 100);
+        pop();
+
+        push();
+        fill("#52d5d5ff");
+        noStroke();
+        ellipse(700, 260, 80, 80);
+        pop();
+    }
+
+
+    /**draw the eyes of the creature */
+    function drawEyes() {
+        //draw eye 1
+        push();
+        fill("#ffffffff");
+        noStroke();
+        ellipse(675, 255, 25, 25);
+        pop();
+        //draw inside eyes 1
+        push();
+        fill("#000000ff");
+        noStroke();
+        ellipse(675, 255, 15, 15);
+        pop();
+
+        //draw eye 2
+        push();
+        fill("#ffffffff");
+        noStroke();
+        ellipse(725, 255, 25, 25);
+        pop();
+        //draw inside eye 2
+        push();
+        fill("#000000ff");
+        noStroke();
+        ellipse(725, 255, 15, 15);
+        pop();
+    }
+
+    /**bird alien nose */
+    function drawNose() {
+        push();
+        fill("#ffd500ff");
+        noStroke();
+        rect(695, 265, 30, 10);
+        pop();
+    }
+}
+
+
+
+
+
+
+
