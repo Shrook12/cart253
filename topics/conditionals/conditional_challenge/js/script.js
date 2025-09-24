8df2043f8e352c2c7b616fcb08e601ae83168288
Shrook12 <shrook1@gmail.com>
2025-09-24 19:34:27 -0400

    made the puck move to the right when the mouse touch.

--- topics/conditionals/conditional_challenge/js/script.js
+++ topics/conditionals/conditional_challenge/js/script.js
@@ -1,24 +1,85 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Circle Master
+ * Shrook Ahmed
+ *
+ * This will be a program in which the user can push a circle
+ * on the canvas using their own circle.
  */
 
-"use strict";
+const puck = {
+    x: 200,
+    y: 200,
+    size: 100,
+    fill: "#ff0000"
+};
+
+const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "#000000"
+};
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Create the canvas
+ */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
+/**
+ * Move the user circle, check for overlap, draw the two circles
+ */
+function draw() {
+    background("#aaaaaa");
+
+    // Move user circle
+    moveUser();
+    movePuck();
+
+    // Draw the user and puck
+    drawUser();
+    drawPuck();
+}
+
+function movePuck() {
+
+
+    const distance = dist(user.x, user.y, puck.x, puck.y);
+    const mouseIsOverlapping = (distance < puck.size / 2);
+
+    if (mouseIsOverlapping) {
+        puck.x = puck.x + 10;
+    }
+
+
+};
+/**
+ * Sets the user position to the mouse position
+ */
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Displays the user circle
+ */
+function drawUser() {
+    push();
+    noStroke();
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
 
+/**
+ * Displays the puck circle
+ */
+function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
 }
