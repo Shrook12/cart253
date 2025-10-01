bfadf3cb96f98aed372e7417dfc691c7caa0312b
Shrook12 <shrook1@gmail.com>
2025-10-01 01:14:39 -0400

    starting plain javascript events exercise

---
+++ topics/events/plain_js_events/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Plain JavaScript
+ * Shrook Ahmed
+ * 
+ *
+ * Experimenting with event handling in plain JavaScript
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
