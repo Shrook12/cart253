f6ec22b9d7b71e6e4f176d2ffe6075aa702caaae
Shrook12 <shrook1@gmail.com>
2025-10-01 01:07:26 -0400

    finished traffic light exercise

--- topics/events/traffic-light/js/script.js
+++ topics/events/traffic-light/js/script.js
@@ -27,6 +27,10 @@ const trafficLight = {
  */
 function setup() {
     createCanvas(400, 400);
+    /**
+     * display the traffic light
+     */
+    setInterval(changeLight, trafficLight.delay);
 }
 
 /**
@@ -41,4 +45,19 @@ function draw() {
     fill(trafficLight.fill);
     ellipse(trafficLight.x, trafficLight.y, trafficLight.size);
     pop();
+}
+/**
+ * Called when the timer finishes
+ */
+function changeLight() {
+    if (trafficLight.fill === trafficLight.fills.go) {
+        trafficLight.fill = trafficLight.fills.slow;
+    } else if
+        (trafficLight.fill === trafficLight.fills.slow) {
+        trafficLight.fill = trafficLight.fills.stop;
+    }
+    else if (trafficLight.fill === trafficLight.fills.stop) {
+        trafficLight.fill = trafficLight.fills.go;
+    }
+
 }
