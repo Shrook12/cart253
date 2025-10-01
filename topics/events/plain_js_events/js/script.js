ce38d398d6888044749ac269aa459eb708ea16f2
Shrook12 <shrook1@gmail.com>
2025-10-01 01:30:51 -0400

    finished plain javascript events exercise

--- topics/events/plain_js_events/js/script.js
+++ topics/events/plain_js_events/js/script.js
@@ -8,17 +8,45 @@
 
 "use strict";
 
+// information about the current and possible background fills
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#ffffff"
+    },
+    switchKey: 32 // space bar
+}
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the Canvas
 */
 function setup() {
+    createCanvas(400, 400);
 
+    //listen for keypresses
+    window.addEventListener("keydown", changeBG);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the background
 */
 function draw() {
+    background(bg.fill);
 
+}
+
+/**
+ * Switches the background from black to white
+ */
+
+function changeBG(event) {
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 }
