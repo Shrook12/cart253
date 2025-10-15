759652a5328b9cd5ee59a936d977bffcf939866d
Shrook12 <shrook1@gmail.com>
2025-10-15 12:51:54 -0400

    Did a canva with the full window size, and the three different screen state. Now I am trying to make the start screen change into game screen when a button is pressed. I discovered that I love windowWidth and windowHeight!

--- topics/projects/mod_jam/mod_jam_shrook/js/try.js
+++ topics/projects/mod_jam/mod_jam_shrook/js/try.js
@@ -1,3 +1,10 @@
+"use strict";
+
+
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
 /**
  * Title of Project
  * Author Name
@@ -8,16 +15,45 @@
 
 "use strict";
 
+let finishState = "none";
+let gameState = "start";
+let score = 0;
+
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
 
 }
-
-
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    if (gameState == "start") {
+        startScreen();
+    }
+    else if (gameState == "play") {
+        gameScreen();
+    }
+    else if (gameState == "end") {
+        endScreen();
+    }
 }
+
+function startScreen() {
+    background("#a545ffff");
+
+}
+
+function gameScreen() {
+    background("#4577ffff");
+}
+
+function endScreen() {
+    background("#ff456aff");
+}
+
+
