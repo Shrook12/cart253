68ce665a079a29e4941732f8c665f5322f363042
Shrook12 <shrook1@gmail.com>
2025-11-11 21:07:21 -0500

    finished tarot exercise

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -6,12 +6,18 @@
  */
 
 "use strict";
+//our tarot data
+let tarot = undefined;
 
+let fortune = "Click to show a fortune.";
 /**
  * tbd.
 */
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
+}
 function setup() {
-
+    createCanvas(800, 400);
 }
 
 
@@ -19,5 +25,21 @@ function setup() {
  * tbd.
 */
 function draw() {
+    background(0);
 
+
+
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    //choose a random card
+    const card = random(tarot.tarot_interpretations);
+    //choose a random fortune
+    fortune = random(card.fortune_telling)
 }
