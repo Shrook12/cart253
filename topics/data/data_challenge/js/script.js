cb32e22f3c0e631da0e2ad8295a5fb1b0b05230c
Shrook12 <shrook1@gmail.com>
2025-11-12 20:58:29 -0500

    finished data challenge

--- topics/data/data_challenge/js/script.js
+++ topics/data/data_challenge/js/script.js
@@ -1,24 +1,80 @@
 /**
- * Title of Project
- * Author Name
+ * Terrible New Car
+ * Pippin Barr
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A program to generate new car model names using dinosaurs.
+ * 
+ * Uses:
+ * Darius Kazemi's corpora repository
+ * https://github.com/dariusk/corpora/tree/master
  */
 
 "use strict";
 
+let carData = undefined;
+let dinosaurData = undefined;
+let langData = undefined;
+let lang = "fr";
+
+
+
+// Starts with the instruction
+let carName = undefined;
+let dinosaurName = "";
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+ * Load the car, dinosaur and language data
+ */
+function preload() {
+    carData = loadJSON("assets/data/cars.json");
+    dinosaurData = loadJSON("assets/data/dinosaurs.json");
+    langData = loadJSON("assets/data/lang.json");
+
 
 }
 
 
+
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Create the canvas
+*/
+
+function setup() {
+    createCanvas(600, 400);
+    // if language is fr then take the french instruction from the lang.js
+    if (lang === "fr") {
+        carName = langData.language.fr;
+    }
+    // if language is fr then take the english instruction from the lang.js
+    else if (lang === "en") {
+        carName = langData.language.en;
+    }
+}
+
+/**
+ * Display the current main text (either instructions or a car)
 */
 function draw() {
+
+
+    background(0);
+
+    push();
+    fill("pink");
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(carName + dinosaurName, width / 2, height / 2);
+    pop();
+}
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {
+    //choose a random car
+    carName = random(carData.cars);
+    //choose a random dinosaur
+    dinosaurName = random(dinosaurData.dinosaurs);
+
 
 }
